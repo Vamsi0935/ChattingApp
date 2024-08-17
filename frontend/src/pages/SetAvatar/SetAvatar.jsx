547acdbc078ffaa3e91df3f7fd24cfc11afdf3c2
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-08-17 16:28:49 +0530

    Update SetAvatar.jsx

--- frontend/src/pages/SetAvatar/SetAvatar.jsx
+++ frontend/src/pages/SetAvatar/SetAvatar.jsx
@@ -54,7 +54,7 @@ export default function SetAvatar() {
           localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
         );
         const { data } = await axios.post(
-          `http://localhost:5000/api/auth/setavatar/${user._id}`,
+          `https://chatting-app-api.vercel.app/api/auth/setavatar/${user._id}`,
           {
             image: avatars[selectedAvatar],
           }
